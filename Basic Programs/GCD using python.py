9b662c4443eb1662d71dc6c5ea6c8daccc8fd5c6
ManjushaTVS <69451319+ManjushaTVS@users.noreply.github.com>
2020-10-20 22:08:45 +0530

    update_GCD.py

--- Basic Programs/GCD using python.py
+++ Basic Programs/GCD using python.py
@@ -1,14 +1,15 @@
-#GCD calculation of any two inputted numbers
-def Greatest_Common_Divisor(a,b):
-    print("For numbers",a,"and",b)
-    while b!=0:
-        #swapping using another variable
-        temp=b
-        b=a%b
-        a=temp
-    gcd=a
-    print("GCD is:",gcd)
-print("Enter the numbers: \n")
-num1=int(input())
-num2=int(input())
-Greatest_Common_Divisor(num1,num2)        
+def calculategcd(x, y):
+    if x > y: 
+        small = y 
+    else: 
+        small = x 
+    for i in range(1, small+1): 
+        if((x % i == 0) and (y % i == 0)): 
+            gcd = i               
+    return gcd 
+def lcm(x,y):
+    return (x//calculategcd(x, y))* y
+a =int(input("enter first number:"))
+b= int(input("enter second number:"))
+print ("The gcd of given numbers is : ",calculategcd(a,b)) 
+print("The lcm of given numbers is : ",lcm(a,b))
